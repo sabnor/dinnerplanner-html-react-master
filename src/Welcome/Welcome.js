5c8b45406cc2d32e797ca9411272d2f0c77f5c66
Sabina Nordell <sabinanordell@n154-p188.eduroam.kth.se>
2018-05-22 18:55:42 +0200

    WelcomeView

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -5,14 +5,15 @@ import { Link } from 'react-router-dom';
 class Welcome extends Component {
   render() {
     return (
-      <div className="Welcome">
-        <p>
-            Welcome to the dinner planner React Startup code!
-        </p>
+      <div className="">
+          {/*/ <img className="imgWelcome" src="https://buyer-static.postmates.com/dist/prod/d1cec2a0ebfc3b4eb296a9d3d85aa76a.png"></img>*/}
+        <div className="text-center">
+          <p className='textSquare'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
         
         <Link to="/search">
-            <button>Start planning</button>
+          <button className="btn btn-warning">Create new dinner</button>
         </Link>
+        </div>
       </div>
     );
   }
