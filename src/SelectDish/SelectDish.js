991ad6c955d21fd79b8e08984795ba7142725bf9
Karolin Valaszkai <karolinv@kth.se>
2018-05-23 15:07:59 +0200

    k

--- src/SelectDish/SelectDish.js
+++ src/SelectDish/SelectDish.js
@@ -8,20 +8,19 @@ import Dishes from '../Dishes/Dishes';
 class SelectDish extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'starter'};
+    this.state = {type: 'starter', filter: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({type: event.target.value, filter:event.target.value});
     event.preventDefault();
   }
 
   handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    this.setState({value: event.target.value});
+    this.setState({type: event.target.value, filter:event.target.value});
     event.preventDefault();
   }
 
@@ -33,17 +32,27 @@ class SelectDish extends Component {
         <h2 className="jumbotron-heading">This is the Select Dish screen</h2>
         <p className="lead text-muted">Search for what food u want mate.</p>
       <form>
+        <div className="form-row">
           <select value={this.state.value} onChange={this.handleChange}>
             <option value="starter">Starter</option>
             <option value="side dish">Side dish</option>
             <option value="main course">Main course</option>
             <option value="dessert">Dessert</option>
           </select>
+
+          <div className="col">
+            <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="Filter by dishname"></input>
+          </div>
+
+          <div className="col">
+            <button type="button-center" className="btn btn-warning" onSubmit={this.handleSubmit}>Filter</button>
+          </div>
+        </div>  
       </form>
 
         {/* We pass the model as property to the Sidebar component */}
         <Sidebar model={this.props.model}/>
-        <Dishes type={this.state.value}/>
+        <Dishes type={this.state.type} filter={this.state.filter}/>
       </div>
     );
   }
