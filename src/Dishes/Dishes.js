f366df301443f9ed7b7fb9febf8a894816c18ee6
Sabina Nordell <sabinanordell@n154-p188.eduroam.kth.se>
2018-05-23 11:36:34 +0200

    bootstrap thumbnails

--- src/Dishes/Dishes.js
+++ src/Dishes/Dishes.js
@@ -45,10 +45,18 @@ class Dishes extends Component {
         break;
       case 'LOADED':
         dishesList = this.state.dishes.map((dish) =>
-          <div>
+          <div key={dish.id} className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img className="card-img-top" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1638c4fcd6b%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1638c4fcd6b%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.9375%22%20y%3D%22117.45%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true"/>
+              <div className="card-body">
+                  <p className="card-text">{dish.title}</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+            {/* <h3 key={dish.id}>{dish.title}</h3> */}
 
-            <h3 key={dish.id}>{dish.title}</h3>
-
+            </div>
           </div>
         )
         break;
@@ -61,7 +69,7 @@ class Dishes extends Component {
       <div className="Dishes">
         <h3>Dishes</h3>
         <div>
-          
+
           {dishesList}
         </div>
       </div>
