49df2ba7b23d1e204345d90df13bf92f5407ef31
Sabina Nordell <sabinanordell@n154-p188.eduroam.kth.se>
2018-05-23 10:24:45 +0200

    Api-key

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -1,5 +1,5 @@
 const httpOptions = {
-  headers: {'X-Mashape-Key': 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB'}
+  headers: {'X-Mashape-Key': 'k2x8zD2tl2mshzMW9pmsxLv8y7Elp1bJxTJjsnm11xhlvs7qCX'}
 };
 
 const DinnerModel = function () {
