b7fa6057f857a9cd861b24b87786fe437713ba6d
Sabina Nordell <sabinanordell@n188-p205.eduroam.kth.se>
2018-05-24 10:28:44 +0200

    dish/:id

--- src/Dish.js
+++ src/Dish.js
@@ -9,8 +9,8 @@ class Dish extends Component {
           {/*/ <img className="imgWelcome" src="https://buyer-static.postmates.com/dist/prod/d1cec2a0ebfc3b4eb296a9d3d85aa76a.png"></img>*/}
         <div className="text-center">
           <p className='textSquare lead text-muted'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-{        this.props.id
-}        {/* <Link to="/search">
+          {this.props.id}        
+          {/* <Link to="/search">
           <button className="btn btn-warning">Create new dinner</button>
         </Link> */}
         {/* <Sidebar model={this.props.model}/> */}
